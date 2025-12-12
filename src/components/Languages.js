1dafdf8b068e1fda8f028dd50cc1712728618558
Zerhouni Omar <hddjdndjdndj@gmail.com>
2025-12-12 15:36:58 +0100

    Fix Remove duplicate language entries in Languages.js

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import PixelTransition from './PixelTransition';
